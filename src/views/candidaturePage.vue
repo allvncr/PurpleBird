4f890925cd55c36e1bbf9a34d63473e92ec241bb
Loic-tech <74209385+Loic-tech@users.noreply.github.com>
2022-12-30 00:31:55 +0100

    push

---
+++ src/views/candidaturePage.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "candidaturePage"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/candidatureSpontannee.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "candidatureSpontannee"
+}
+</script>
+
+<style scoped>
+
+</style>
